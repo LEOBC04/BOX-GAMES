6d67a1dcc9474074f6b2d889f2ddc93a273adcfa
Leandro <lbuitragoc04@gmail.com>
2022-07-07 20:44:04 -0500

    deploy

--- api/index.js
+++ api/index.js
@@ -20,11 +20,11 @@
 const server = require("./src/app.js");
 const { genres } = require("./src/controllers/genreController");
 const { conn } = require("./src/db.js");
-// const { serverPort } = require("./src/utils/config/index");
+let port = process.env.PORT || 3001
 
 conn.sync({ force: false }).then(() => {
   console.log("Database connected");
-  server.listen(process.env.PORT, async () => {
+  server.listen(port, async () => {
     console.log('Server listening on port 3001');
     await genres();
   });
