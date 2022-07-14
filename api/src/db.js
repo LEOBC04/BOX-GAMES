9a1edb6b90b0cb82f5a1120b1caed12d0a349e4c
Leandro <lbuitragoc04@gmail.com>
2022-07-14 14:44:21 -0500

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize, Op } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const { dbUser, dbPassword, dbHost, dbPort, dbName } = require('./utils/config/index');
-const { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD } = process.env;
+const { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD  } = process.env;
 
 let sequelize = 
   process.env.NODE_ENV === "production"
@@ -29,7 +29,7 @@ let sequelize =
       },
       ssl: true,
     })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
